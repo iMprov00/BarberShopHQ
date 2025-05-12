639ce1ce947e12bc9c9021e5c0e2729f218ecf0e
iMprov <improv00@mail.ru>
2025-05-12 20:46:46 +0700

    Добавлена строка подключения к БД + инициализация класса "Клиента"

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db" #строка подключения к бд
+
+class Client < ActiveRecord::Base #инициализация класса клиент
+
+end
 
 
 get '/' do 
